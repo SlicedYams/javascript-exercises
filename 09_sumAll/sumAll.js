afade53e3746d185cf198a4d163b054976d08f8a
SlicedYams <MDRockets01@gmail.com>
2025-05-28 20:19:01 -0500

    Finish exericse to pass all tests

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(smallerNumber, biggerNumber) {
 
+    if(typeof smallerNumber != "number" || typeof biggerNumber != "number") {
+        return "ERROR";
+    }
+    if(smallerNumber % 1 != 0 || biggerNumber % 1 != 0 || smallerNumber < 0 || biggerNumber < 0) {
+        return "ERROR";
+    }
+    if(smallerNumber > biggerNumber) {
+        let temp = smallerNumber;
+        smallerNumber = biggerNumber;
+        biggerNumber = temp;
+    }
+    let sum = 0;
+    for(let i = smallerNumber; i <= biggerNumber; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
