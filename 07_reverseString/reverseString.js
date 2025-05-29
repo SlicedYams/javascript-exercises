8a8b1c06bc629b40356d7a52fac769044815070c
SlicedYams <MDRockets01@gmail.com>
2025-05-28 19:36:02 -0500

    Finish exercise to pass all tests

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitString = str.split("");
+    let finalString = '';
+    for(let i = splitString.length - 1; i >= 0; i--) {
+        finalString += splitString[i];
+    }
+    return finalString;
 };
 
 // Do not edit below this line
