c0299b48d922b035c7d37e13057595a6e55f99ed
SlicedYams <MDRockets01@gmail.com>
2025-05-28 19:59:16 -0500

    Finish the exercise to pass all tests

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    let indexOfItem;
+    for(let i = 1; i < arguments.length; i++) {
+        let findIndexFunction = (item) => item === arguments[i];
+        while(arr.findIndex(findIndexFunction) !== -1) {
+            indexOfItem = arr.findIndex(findIndexFunction);
+            arr.splice(indexOfItem, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
