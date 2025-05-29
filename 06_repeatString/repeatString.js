5a8b6379c6666a03e2dac4dddc6cf902d6940532
SlicedYams <MDRockets01@gmail.com>
2025-05-28 19:29:53 -0500

    Finish exercise to pass all tests

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let finalString = '';
+    if(num < 0) {
+        return "ERROR";
+    }
+    for(let i = 1; i <= num; i++) {
+        finalString += str;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
