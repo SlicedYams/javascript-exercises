d5886ca411fee692e19273fab8f77941ff531ba3
SlicedYams <MDRockets01@gmail.com>
2025-05-28 21:12:07 -0500

    Finish exercise to pass all tests

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let toCelsius = (num - 32) * (5/9);
+  if(toCelsius % 1 != 0) {
+    return parseFloat(toCelsius.toFixed(1));
+  }
+  return toCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let toFahrenheit = num * (9/5) + 32;
+  if(toFahrenheit % 1 != 0) {
+    return parseFloat(toFahrenheit.toFixed(1));
+  }
+  return toFahrenheit;
 };
 
 // Do not edit below this line
